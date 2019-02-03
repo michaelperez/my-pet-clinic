e6b41d9dc1b30a38c2c4086eff25c1f1cb1d3307
michaelperez <michael.perez@yahoo.com>
2019-02-03 10:37:54 -0600

    Initial import of project into GitHub

---
+++ src/main/java/com/perezma/mypetclinic/MyPetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.perezma.mypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPetClinicApplication.class, args);
+    }
+
+}
+
