9e241d7a2e8441a5efa2bc751a19c04a85ba9d5f
michaelperez <michael.perez@yahoo.com>
2019-02-07 18:56:54 -0600

    Added alternative request mapping at class level. All methods in class can omit the directory prefix in the mapping.

--- pet-clinic-web/src/main/java/com/perezma/mypetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/perezma/mypetclinic/controllers/OwnerController.java
@@ -3,10 +3,12 @@ package com.perezma.mypetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    //@RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    @RequestMapping({"", "/", "index", "index.html"})
     public String listOwners() {
         return "/owners/index";
     }
