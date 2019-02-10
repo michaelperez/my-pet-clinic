4f19e13bdac270df8d5b388c2606bb67f4d92414
michaelperez <michael.perez@yahoo.com>
2019-02-09 19:55:09 -0600

    added Specialty model, updated Vet model with Specialty relationship. Closes #27.

---
+++ pet-clinic-data/src/main/java/com/perezma/mypetclinic/models/Specialty.java
@@ -0,0 +1,14 @@
+package com.perezma.mypetclinic.models;
+
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
