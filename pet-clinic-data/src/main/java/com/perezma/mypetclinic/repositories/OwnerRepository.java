99364b34b9801c7d409424314b4704ed3b9b2e20
michaelperez <michael.perez@yahoo.com>
2019-02-10 19:42:04 -0600

    Implemented spring data jpa for Owner. Closes #40.

--- pet-clinic-data/src/main/java/com/perezma/mypetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/perezma/mypetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import com.perezma.mypetclinic.models.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
