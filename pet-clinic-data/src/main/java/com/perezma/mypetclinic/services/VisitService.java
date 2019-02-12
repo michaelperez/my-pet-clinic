0fa584137a733dce9df09e1ce9fa85db80b7bffc
michaelperez <michael.perez@yahoo.com>
2019-02-11 20:45:40 -0600

    Implemented Visit Map Service. Closes #46.

---
+++ pet-clinic-data/src/main/java/com/perezma/mypetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.perezma.mypetclinic.services;
+
+import com.perezma.mypetclinic.models.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/perezma/mypetclinic/services/map/VisitMapService.java
@@ -0,0 +1,52 @@
+package com.perezma.mypetclinic.services.map;
+
+import com.perezma.mypetclinic.models.Visit;
+import com.perezma.mypetclinic.services.OwnerService;
+import com.perezma.mypetclinic.services.PetService;
+import com.perezma.mypetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    private final OwnerService ownerService;
+    private final PetService petService;
+
+    public VisitMapService(OwnerService ownerService, PetService petService) {
+        this.ownerService = ownerService;
+        this.petService = petService;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deletebyId(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        //defensive coding to verify we have a proper Visit object.
+        if (visit.getPet() == null || visit.getPet().getOwner() == null ||
+                visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
