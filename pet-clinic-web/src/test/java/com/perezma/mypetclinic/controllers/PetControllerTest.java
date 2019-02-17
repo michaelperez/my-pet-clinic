75ad05a2def319f175ec5c11dba85a352d7ac30e
michaelperez <michael.perez@yahoo.com>
2019-02-16 21:31:02 -0600

    Fixed Pet form. Closes #56

---
+++ pet-clinic-web/src/test/java/com/perezma/mypetclinic/controllers/PetControllerTest.java
@@ -0,0 +1,138 @@
+package com.perezma.mypetclinic.controllers;
+
+import com.perezma.mypetclinic.models.Owner;
+import com.perezma.mypetclinic.models.Pet;
+import com.perezma.mypetclinic.models.PetType;
+import com.perezma.mypetclinic.services.OwnerService;
+import com.perezma.mypetclinic.services.PetService;
+import com.perezma.mypetclinic.services.PetTypeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class PetControllerTest {
+
+    @Mock
+    PetService petService;
+
+    @Mock
+    OwnerService ownerService;
+
+    @Mock
+    PetTypeService petTypeService;
+
+    @InjectMocks
+    PetController petController;
+
+    MockMvc mockMvc;
+
+    Owner owner;
+    Set<PetType> petTypes;
+
+    @BeforeEach
+    void setUp() {
+        owner = new Owner();
+        owner.setId(1L);
+
+        PetType petType1 = new PetType();
+        petType1.setId(1L);
+        petType1.setName("Dog");
+
+        PetType petType2 = new PetType();
+        petType2.setId(2L);
+        petType2.setName("Cat");
+
+        petTypes = new HashSet<>();
+        petTypes.add(petType1);
+        petTypes.add(petType2);
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(petController)
+                .build();
+    }
+
+    @Test
+    void initCreationForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+
+        mockMvc.perform(get("/owners/1/pets/new"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(model().attributeExists("pet"))
+                .andExpect(view().name("pets/createOrUpdatePetForm"));
+    }
+
+    @Test
+    void processCreationForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+
+        mockMvc.perform(post("/owners/1/pets/new"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+
+        verify(petService).save(any());
+    }
+
+    @Test
+    void initUpdateForm() throws Exception {
+        Pet pet2 = new Pet();
+        pet2.setId(2L);
+
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+        when(petService.findById(anyLong())).thenReturn(pet2);
+
+        mockMvc.perform(get("/owners/1/pets/2/edit"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(model().attributeExists("pet"))
+                .andExpect(view().name("pets/createOrUpdatePetForm"));
+    }
+
+    @Test
+    void processUpdateForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+
+        mockMvc.perform(post("/owners/1/pets/2/edit"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+
+        verify(petService).save(any());
+    }
+
+    @Test
+    void populatePetTypes() {
+        //todo impl
+    }
+
+    @Test
+    void findOwner() {
+        //todo impl
+    }
+
+    @Test
+    void initOwnerBinder() {
+        //todo impl
+    }
+}
